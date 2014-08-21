4d6a704945cba229bee6b1064106787e8fa3b591
rraman <rr@chargebee.com>
2014-08-21 22:59:25 +0530

    Removing unused file.

--- php/lib/ChargeBee/Models/InvoiceEstimatedLineItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ChargeBee_InvoiceEstimatedLineItem extends ChargeBee_Model
-{
-  protected $allowed = array('date_from', 'date_to', 'unit_amount', 'quantity', 'tax', 'tax_rate', 'amount', 'description', 'type', 'entity_type', 'entity_id');
-
-}
-
-?>
