ba5eb6584278781b785394d4689926db661966e3
rraman <rr@chargebee.com>
2014-09-09 23:26:32 +0530

    Handling the response error type.

--- dotnet/ChargeBee/Api/ApiException.cs
+++ dotnet/ChargeBee/Api/ApiException.cs
@@ -1,75 +1,56 @@
 ﻿using System;
 using System.Net;
 using System.Runtime.Serialization;
+using System.Collections.Generic;
 
 namespace ChargeBee.Api
 {
     public class ApiException : ApplicationException
     {
-        public ApiException(SerializationInfo info, StreamingContext context)
-        {
-            if (info != null)
-            {
-                foreach (var item in info)
-                {
-                    switch (item.Name)
-                    {
-                        case "code":
-                            Code = item.Value.ToString();
-                            break;
-                        case "param":
-                            Param = item.Value.ToString();
-                            Parameter = Param;
-                            break;
-                        case "msg":
-                            Msg = item.Value.ToString();
-                            ApiMessage = Msg;
-                            break;
-                        case "type":
-                            Type = item.Value.ToString();
-                            break;
-                        case "error_code":
-                            ApiCode = item.Value.ToString();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        }
 
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
+		private string ErrorParam = "";
 
-            if (info != null)
-            {
-                info.AddValue("type", Type);
-                info.AddValue("code", Code);
-                info.AddValue("param", Param);
-                info.AddValue("msg", Msg);
-                info.AddValue("error_code", ApiCode);
-            }
+		public ApiException(HttpStatusCode httpStatusCode, Dictionary<string, string> errorResp)
+        {
+			this.HttpStatusCode = httpStatusCode;
+			this.Type = errorResp ["type"];
+			this.Code = errorResp ["code"];
+			this.ApiCode = errorResp ["error_code"];
+			this.ApiMessage = errorResp ["error_msg"];
+			this.Msg = errorResp ["msg"];
+			errorResp.TryGetValue("param", out ErrorParam);
         }
 
         public HttpStatusCode HttpStatusCode { get; set; }
 
+		public string Type { get; set; }
+
         public string Code { get; set; }
 
-        public string Param { get; set; }
+        public string Param { 
+			get {
+				return this.ErrorParam;
+			}
+		}
 
         public string Msg { get; set; }
 
-        public string Type { get; set; }
-
         [System.Obsolete("Use HttpStatusCode")]
-        public HttpStatusCode HttpCode { get; set; }
+        public HttpStatusCode HttpCode { 
+			get {
+				return HttpStatusCode;
+			}
+		}
 
         [System.Obsolete("Use Code")]
-        public string ApiCode { get; set; }
+		public string ApiCode { get; set; }
 
         [System.Obsolete("Use Param")]
-        public string Parameter { get; set; }
+		public string Parameter { 
+			get { 
+				return Param;
+			} 
+		}
 
         [System.Obsolete("Use Msg")]
         public string ApiMessage { get; set; }
