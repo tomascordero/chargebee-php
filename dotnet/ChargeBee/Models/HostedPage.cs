3563ea1e156558dfb614bc86d448f42cb5566e5f
abinaya <abinaya@chargebee.com>
2014-12-19 19:56:55 +0530

    commit client libraries

--- dotnet/ChargeBee/Models/HostedPage.cs
+++ dotnet/ChargeBee/Models/HostedPage.cs
@@ -365,12 +365,6 @@ namespace ChargeBee.Models
                 m_params.Add("customer[id]", customerId);
                 return this;
             }
-            [Obsolete]
-            public UpdatePaymentMethodRequest CustomerVatNumber(string customerVatNumber) 
-            {
-                m_params.AddOpt("customer[vat_number]", customerVatNumber);
-                return this;
-            }
             public UpdatePaymentMethodRequest CardGateway(GatewayEnum cardGateway) 
             {
                 m_params.AddOpt("card[gateway]", cardGateway);
