0365ed05bd0b16bb5d5065bc3965cba4ed5377e1
SangeethaBaskaran <sangeetha@chargebee.com>
2016-12-29 13:37:09 +0530

    pull request changes

--- php/lib/ChargeBee/Result.php
+++ php/lib/ChargeBee/Result.php
@@ -186,6 +186,11 @@ class ChargeBee_Result
         }
         return $this->_responseObj[$type];
     }
+    
+    public function getResponse()
+    {
+        return $this->_response;
+    }
 
 }
 
